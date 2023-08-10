c0d382155c0117635a8f91f822a26bbfcac311ed
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-10 16:57:24 +0100

    Created post model

--- blog/models.py
+++ blog/models.py
@@ -93,12 +93,26 @@ class Author(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=50)
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=50)
 
 
 class Keyword(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
+    keywords = models.ManyToManyField(Keyword)
+
+    def __str__(self):
+        return self.title
