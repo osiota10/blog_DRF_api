06dcf9c0ed8cac346e7a8e557d7da2676d087e32
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-11 13:48:14 +0100

    Category serializer

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializer import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from djoser.views import UserViewSet
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+
+class CategoryView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny,]
