be6bba57bd2ea2c23f938b6581b862a10528113d
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-11 13:30:02 +0100

    Djoser urls

--- blog_drf_api/urls.py
+++ blog_drf_api/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
