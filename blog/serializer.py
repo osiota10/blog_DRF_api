3c55a13580f2cf2471bc670d99c1f1615703da27
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-11 14:55:03 +0100

    Created Tag serializer

--- blog/serializer.py
+++ blog/serializer.py
@@ -10,3 +10,17 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+
+class PostSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'content', 'pub_date', 'slug', 'category')
