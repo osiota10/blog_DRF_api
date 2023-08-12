1457b1e2f9642fd2cc55891ef8d0afa891296725
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-12 17:53:05 +0100

    Public users post detail view url parameter

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('category', CategoryView.as_view()),
     path('post-list', PostListView.as_view()),  # Public users
+    path('post-list/<slug:slug>', PostDetailView.as_view()),  # Public users
     path('post', PostView.as_view()),  # Authors
 ]
