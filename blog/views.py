727abbae471c093b1ea458e22db88637786514a5
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-12 17:35:51 +0100

    Post view post method

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,23 @@ class PostView(APIView):
         pass
 
     def post(self, request, *args, **kwargs):
-        pass
+        title = request.data.get('title')
+        content = request.data.get('content')
+        category = request.data.get('category')
+        tags = request.data.get('tags')
+        keywords = request.data.get('keywords')
+
+        if not title or not content or not category:
+            return Response({'error': 'Title, Content and Category are required fields.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            post = Post.objects.create(
+                user=request.user, title=title, content=content, category=category, tags=tags, keywords=keywords)
+
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def delete(self, request, *args, **kwargs):
         pass
