6f7e3d9c005d909910d0510f28eb6a724b17f0aa
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-08-18 21:30:18 +0100

    Created Like model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.core.validators import RegexValidator
 from django.conf import settings
 from cloudinary.models import CloudinaryField
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class UserAccountManager(BaseUserManager):
@@ -131,3 +133,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user} on {self.post} at {self.created_at}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by {self.user} on {self.content_object} at {self.created_at}"
