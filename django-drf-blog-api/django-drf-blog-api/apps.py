a52f5541645d34a561c2365000527877ba0ba681
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-12-16 15:19:14 +0100

    Version 0.0.7

--- django-drf-blog-api/django-drf-blog-api/apps.py
+++ django-drf-blog-api/django-drf-blog-api/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_drf_blog_api'
+    name = 'django-drf-blog-api'
 
     def ready(self):
         pass
